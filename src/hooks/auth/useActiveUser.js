a6e2d225176e3471cee567985de1de99167ec1bd
Lutfi Andriyanto <lutfiandri232@gmail.com>
2021-10-03 09:55:43 +0700

    docs: active user hook

--- src/hooks/auth/useActiveUser.js
+++ src/hooks/auth/useActiveUser.js
@@ -3,6 +3,9 @@ import { setActiveUser } from '../../redux/user';
 import { auth } from '../../firebase';
 import { useState, useEffect } from 'react';
 
+/**
+ * Used for check user that signed in on this app after reload the browser
+ */
 export default function useActiveUser() {
   const dispatch = useDispatch();
 
